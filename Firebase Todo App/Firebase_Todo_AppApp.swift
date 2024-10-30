39b6629ce826d97fb954324a2e330c6970f99911
centinna-l <jerryjoy99@gmail.com>
2024-10-30 01:38:13 -0400

    Initial Commit

---
+++ Firebase Todo App/Firebase_Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_Todo_AppApp.swift
+//  Firebase Todo App
+//
+//  Created by Jerry Joy on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
